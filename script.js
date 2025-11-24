9ff4d7cb3b0865c12b121fcc3bf10224fea54116
magieamigos-source <magieamigos@gmail.com>
2025-11-24 12:51:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const words1 = [
+    "soft", "moon", "pink", "angel", "dreamy", "velvet", "cloudy", "vibe", 
+    "sunny", "peachy", "shadow", "rosy", "quiet", "vintage", "midnight"
+];
 
+const words2 = [
+    "blossom", "vibes", "star", "kitty", "heart", "rose", "skies",
+    "aura", "whisper", "dreams", "sparkle", "lover", "mist", "smile"
+];
+
+function generateUsername() {
+    const part1 = words1[Math.floor(Math.random() * words1.length)];
+    const part2 = words2[Math.floor(Math.random() * words2.length)];
+    const number = Math.floor(Math.random() * 999);
+
+    const username = part1 + part2 + number;
+
+    document.getElementById("output").innerText = username;
+}
